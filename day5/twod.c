35e02557ea4dc05458dad96dc5efb5c339cf2ad3
pajwal <prajwaldivyanshu@gmail.com>
2025-08-08 20:13:11 +0530

    test

---
+++ day5/twod.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+
+    int arr1[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr2[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int result[3][3] = {0};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            result[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                result[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d\t", result[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
